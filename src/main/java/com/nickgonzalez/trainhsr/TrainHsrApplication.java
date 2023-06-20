be697170dd9eee5bd96345c22907fc6340c6634c
Nicholas Gonzalez <thisnickg194@gmail.com>
2023-06-20 09:52:31 -0500

    init

---
+++ src/main/java/com/nickgonzalez/trainhsr/TrainHsrApplication.java
@@ -0,0 +1,13 @@
+package com.nickgonzalez.trainhsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainHsrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainHsrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nickgonzalez/trainhsr/TrainHsrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickgonzalez.trainhsr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainHsrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
